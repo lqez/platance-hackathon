d354b3f334dbcd30f0ae2981f0e05186566d198d
Park Hyunwoo <ez@smartstudy.co.kr>
2016-09-10 21:00:19 +0900

    Improve path display

--- webapp/cav/views.py
+++ webapp/cav/views.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render
 from models import DeliveryTime, Order
-from random import randint
+from random import shuffle
+import os
+import subprocess
 
 
 def index(request):
@@ -36,18 +39,52 @@ def simulate(request):
     riders = int(request.GET.get('riders', 1))
 
     deliveryTime = date + ' ' + time
+    orders = list(Order.objects.filter(deliveryTime=deliveryTime))
+
+    # Oh shit my hard-coded switch cases...
     result = []
+    if algorithm == 'random':
+        def chunkIt(seq, num):
+            avg = len(seq) / float(num)
+            out = []
+            last = 0.0
 
-    orders = Order.objects.filter(deliveryTime=deliveryTime)
+            while last < len(seq):
+                out.append(seq[int(last):int(last + avg)])
+                last += avg
 
-    for order in orders:
-        result.append({
-            'latitude': order.latitude,
-            'longitude': order.longitude,
-            'count': len(order.menuorder_set.all()),
-            'color': randint(1, riders),
-        })
+            return out
+
+        shuffle(orders)
+        result = chunkIt(orders, riders)
+    elif algorithm == 'kmean':
+        py = os.path.dirname(settings.BASE_DIR) + '/engine/kmean/clustering.py'
+        text = "{}\n{}\n".format(riders, len(orders))
+        for order in orders:
+            text += "{} {}\n".format(order.latitude, order.longitude)
+        proc = subprocess.Popen(['python', py], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        proc.stdin.write(text)
+        proc.stdin.close()
+
+        outputs = proc.stdout.read()
+        for line in outputs.strip().split("\n"):
+            order_list = []
+            for idx in line.strip().split(" "):
+                order = orders[int(idx)]
+                order_list.append(order)
+            result.append(order_list)
+        proc.wait()
+
+    final = []
+    for path in result:
+        arr = []
+        for obj in path:
+            arr.append({
+                'latitude': obj.latitude,
+                'longitude': obj.longitude,
+            })
+        final.append(arr)
 
     return JsonResponse({
-        'result': result,
+        'result': final,
     })
