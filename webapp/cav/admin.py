14cc9dabc87f81f845d86004ce0b33b55ae26e24
Park Hyunwoo <ez@smartstudy.co.kr>
2016-09-10 15:24:49 +0900

    Add basic models

--- webapp/cav/admin.py
+++ webapp/cav/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from models import Rider, Menu, Order
 
-# Register your models here.
+
+class RiderAdmin(admin.ModelAdmin):
+    list_display = ['name', 'level', ]
+    search_fields = ['name', ]
+    ordering = ('-id',)
+admin.site.register(Rider, RiderAdmin)
+
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', ]
+    search_fields = ['name', ]
+    ordering = ('-id',)
+admin.site.register(Menu, MenuAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['requestDate', 'requestTime', 'deliveryTime', 'deliveryCompleteTime', ]
+    search_fields = ['requestDate', ]
+    ordering = ('-id',)
+admin.site.register(Order, OrderAdmin)
--- webapp/cav/models.py
+++ webapp/cav/models.py
@@ -2,4 +2,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Rider(models.Model):
+    name = models.CharField(max_length=255)
+    level = models.IntegerField(default=1)
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(default=10000)
+
+
+class Order(models.Model):
+    requestDate = models.DateField(db_index=True)
+    requestTime = models.TimeField()
+
+    deliveryTime = models.DateTimeField()
+    deliveryCompleteTime = models.DateTimeField()
+
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    orderedMenus = models.ManyToManyField(Menu)
